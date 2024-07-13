59477d6433eaee023f55b8b0885e979c98246f6a
alan lima <alan.silva1f@gmail.com>
2024-07-12 22:48:49 -0300

    Melhorando as Classes do Projeto Egresso.

--- EgreExperience/src/main/java/main/root/controller/CadastroController.java
+++ EgreExperience/src/main/java/main/root/controller/CadastroController.java
@@ -3,6 +3,7 @@ package main.root.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,4 +27,6 @@ public class CadastroController {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+	
+	
 }
